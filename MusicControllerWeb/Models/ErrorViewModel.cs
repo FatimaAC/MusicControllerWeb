715beb6d338f3008f9a13cef567b980fd0667f4b
Fatima <fatima.mulla766@gmail.com>
2020-12-29 09:38:09 +0300

    Add project files.

---
+++ MusicControllerWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MusicControllerWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
