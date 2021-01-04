256a3b239303492c14059cedb911a3abae3d5095
Fatima <fatima.mulla766@gmail.com>
2021-01-04 12:08:33 +0300

    Schedule Page

--- MusicControllerWeb/Controllers/OutletController.cs
+++ MusicControllerWeb/Controllers/OutletController.cs
@@ -22,5 +22,9 @@ namespace MusicControllerWeb.Controllers
         {
             return View();
         }
+        public IActionResult Schedule()
+        {
+            return View();
+        }
     }
 }
