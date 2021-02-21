f151f685fbd1137945ced6077d1344b91c377f35
Imran Ahmed Khan <i.ahmed@powerholding-intl.com>
2021-02-21 09:26:28 +0300

    Added new classes

---
+++ MusicController.BL/SharePointFiles/ISharePointFileServices.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MusicController.BL.SharePointFiles
+{
+    public interface ISharePointFileServices
+    {
+        Task<string> SaveFile(IFormFile file, long TrackId);
+    }
+}
